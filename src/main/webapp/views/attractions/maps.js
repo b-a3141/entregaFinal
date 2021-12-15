4700f4e959e386116c6863d0c7538ab1bca4d69d
b-a3141 <69673484+b-a3141@users.noreply.github.com>
2021-12-15 15:45:10 -0300

    Add files via upload
    js de la api mapa

---
+++ src/main/webapp/views/attractions/maps.js
@@ -0,0 +1,22 @@
+let map;
+// Nombre del proyecto para la api turismoTP3 
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.6037345, lng: -58.3837591 },
+    zoom: 15,
+    mapTypeId: 'hybrid',
+    scaleControl: true,
+    fullscreenControlOptions: {
+        position: google.maps.ControlPosition.RIGHT_BOTTOM
+},
+  });
+}
+
+let marker = new google.maps.Marker({
+    position: {
+           lat: -34.6037345,
+           lng: -58.383
+    },
+    map: map,
+    title: 'Viaducto de Segovia'
+});
